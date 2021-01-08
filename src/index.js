a1b6e3b765977870d6df48a3b03f85279c14369f
rishi-live <rishi.uemk9@gmail.com>
2021-01-08 11:35:42 +0530

    task update

--- src/index.js
+++ src/index.js
@@ -44,6 +44,31 @@ app.get('/users/:id', async(req, res) => {
     }
 })
 
+app.patch('/users/:id', async(req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+        // const isValidOperation = updates.every((update) => {
+        //     return allowedUpdates.includes(update)
+        // })
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates' })
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!user) {
+            return res.status(404).send("User not found")
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 app.post('/tasks', async(req, res) => {
     const task = new Task(req.body)
     try {
@@ -78,6 +103,28 @@ app.get('/tasks/:id', async(req, res) => {
         res.send(task)
     } catch (e) {
         res.status(500).send(e)
+    }
+})
+
+app.patch('/tasks/:id', async(req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates' })
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!task) {
+            return res.status(404).send("task not found")
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
     }
 })
 
